bb9d26a8c9ce29be34e9e75cbfc3886283d1c27c
DevX32 <60389339+DevX32@users.noreply.github.com>
2023-09-09 12:52:00 +0000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,7 @@
 fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
-description 'Bridge for pefcl(QBOX'
+description 'Bridge for pefcl(QBOX)'
 author 'originally made by sam shanks and remaked for qbox proj by DevX32'
 server_scripts{ 
     'server.lua',
