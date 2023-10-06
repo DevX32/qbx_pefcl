26439011e0f6ce850363d18fefb28756afcd6b99
DevX32 <60389339+DevX32@users.noreply.github.com>
2023-10-06 11:32:54 +0545

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,18 @@ game 'gta5'
 lua54 'yes'
 description 'Bridge for pefcl(QBOX)'
 author 'originally made by sam shanks and remaked for qbox proj by DevX32'
-server_scripts{ 
+shared_scripts {
+    '@ox_lib/init.lua',
+    '@qbx_core/import.lua',
+}
+modules {
+    'qbx_core:playerdata',
+    'qbx_core:utils'
+}
+
+client_script 'client.lua'
+
+server_scripts { 
     'server.lua',
     'config.lua'
 }
