2043275cb318ab6a8105c9d726bb1a779280f330
DevX32 <dishantacnc389@gmail.com>
2024-03-08 08:37:06 +0545

    better config

--- shared/qbx_pefcl.lua
+++ shared/qbx_pefcl.lua
@@ -1,16 +1,16 @@
 return {
     BusinessAccounts = {
         {
-            JobName = 'police', -- Job Name
-            AccountName = 'Los Santos Police', -- Display name for bank account
-            ContributorRole = 2, -- Minimum role required to contribute to the account
-            AdminRole = 3 -- Minimum role to be able to add/remove money from the account
+            JobName = 'police',                   -- Name of the job associated with the account
+            AccountName = 'Police Department',    -- Display name for the bank account
+            ContributorRole = 2,                  -- Minimum role required to contribute to the account
+            AdminRole = 3                         -- Minimum role required to add/remove money from the account
         },
         {
-            JobName = 'ambulance', -- Job Name
-            AccountName = 'Los Santos EMS', -- Display name for bank account
-            ContributorRole = 2, -- Minimum role required to contribute to the account
-            AdminRole = 3 -- Minimum role to be able to add/remove money from the account
+            JobName = 'ambulance',                 -- Name of the job associated with the account
+            AccountName = 'EMS Department',        -- Display name for the bank account
+            ContributorRole = 2,                  -- Minimum role required to contribute to the account
+            AdminRole = 3                         -- Minimum role required to add/remove money from the account
         }
     }
 }
